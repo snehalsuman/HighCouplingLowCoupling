853543096ed05e34f2eb006cc868dcbb0fbd6fb9
whatmakeart <whatmakeart@gmail.com>
2024-12-02 13:24:56 +0530

    loose coupling and tight coupling

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -2,7 +2,7 @@ package com.loose.coupling;
 
 public class LooseCouplingExample {
     public static void main(String[] args) {
-       MongoDBProvider databaseProvider =new MongoDBProvider();
+       UserDataProvider databaseProvider =new MongoDBProvider();
        UserManager userManager = new UserManager(databaseProvider);
         System.out.printf(userManager.getUserName());
 
